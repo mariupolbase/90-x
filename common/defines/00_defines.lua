9a4afc446ba5c29157b3e80ea7a630fb9fcab432
fedya0210 <168896307+fedya0210@users.noreply.github.com>
2024-05-24 17:49:18 +0300

    Superevent и Россия

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -3229,10 +3229,10 @@ NAI = {
 },
 
 NFocus = {
-	FOCUS_POINT_DAYS = 7,						-- Each point takes a week
+	FOCUS_POINT_DAYS = 1,						-- Each point takes a week
 	FOCUS_PROGRESS_PEACE = 1,					-- Progress during peace
 	FOCUS_PROGRESS_WAR = 1,					-- Progress during war
-	MAX_SAVED_FOCUS_PROGRESS = 10,				-- This much progress can be saved while not having a focus selected
+	MAX_SAVED_FOCUS_PROGRESS = 15,				-- This much progress can be saved while not having a focus selected
 },
 
 NOperatives = {
